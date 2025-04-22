1424f25b4aba593093d4c1d94889d3fd5bede2d5
ignvci00 <marti.medinac@duocuc.cl>
2025-04-22 16:35:01 -0400

    ejercicios python for

---
+++ ejercicioconfor.py
@@ -0,0 +1,59 @@
+#ejercicioconfor 
+
+# cant=int(intput("ingrese la cat de repeticiones "))
+
+# for i in range(cant):
+#     print("repeticion" ,i+1)
+
+
+#     nombre=intput("ingrese su nombre")
+#     edad=input("ingrese su edad ")
+
+#     print("hola",nombre "su edad es" , edad)
+#     print(f"hola{nombre} su edad es {edad}")
+
+
+# num=int(input("ingrese un numero"))
+# #escribir i,"x", j , "=" ,i*j
+# for i in range (1,11):
+#     print(num,"x" , i, "=" ,i*num)
+
+#todas las tablas de multiplicar de 1 al 10
+
+# for i in range (1,11)
+#     print("tabla del", i)
+#     for j in range(1,11)
+#     print(i,"x", j,"=", i*j )
+
+#pedir al usuario la cant de notas y promediarlas
+
+# cant=int(input("ingrese la cantidad de notas"))
+
+# for i in range(cant)
+# print(" ingrese la nota ", i+1)
+# nota=float(input())
+# total=total+nota
+
+# prom=total/cant
+
+
+
+# print(f"su promedio es {round(prom,1)}")
+# print(f"la cantidad de notas sobre 4 fue {notasazules}")
+
+
+# if prom>=4:
+#     print("el alumni aprobo")
+# else:
+#     print("el alumno reprobo") 
+
+
+
+clave=3344
+
+password=int(input("ingrese la pasewore :"))
+
+if password==clave:
+    print("bienvenido al sistema ")
+else:
+    print("erro, calve incorrecta")
